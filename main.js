b4e6640632907e70731944378e1be2e9b6942e08
SliverStrike123 <teh.marvin@gmail.com>
2024-05-06 21:17:23 +0800

    I HATE HTML

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ import { insertstonks, addstock, sellstock } from "./functions.js";
 import fs from 'fs-extra';
 
 
-console.log("Hello World")
+document.getElementById('form').addEventListener('submit', function(){
+    const fname = document.getElementById('fname').value
+    const lname = document.getElementById('lname').value
+
+    console.log(fname)
+})
 
 
