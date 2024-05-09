79046775c79aa6837e72f7b2e4c6aa6f41e0d43c
SliverStrike123 <teh.marvin@gmail.com>
2024-05-09 21:28:31 +0800

    HTML RAHHH

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ import { insertstonks, addstock, sellstock } from "./functions.js";
 import fs from 'fs-extra';
 
 
-document.getElementById('form').addEventListener('submit', function(){
-    const fname = document.getElementById('fname').value
-    const lname = document.getElementById('lname').value
-
-    console.log(fname)
+document.getElementById("button").addEventListener("click", function(){
+    const fname = document.getElementById('fname')
+    const lname = document.getElementById('lname')
+    alert(fname)
 })
 
 
