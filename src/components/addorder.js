f0e022cf7dc9d9ab5da73800d6077c53b783d977
SliverStrike123 <teh.marvin@gmail.com>
2024-07-09 22:12:32 +0800

    Day 9 SPM

--- src/components/addorder.js
+++ src/components/addorder.js
@@ -1,10 +1,48 @@
 import React, { useState } from 'react'
+import '../App.css'
 
-function AddOrderGUI( {onReturn} ){
+const PopUp = ({ message, onClose }) => {
+    return (
+        <div className="Popup-overlay">
+            <div className="Popup-content">
+                <p>{message}</p>
+                <button onClick={onClose}>Close</button>
+            </div>
+        </div>
+    );
+};
+
+function AddOrderGUI({ onReturn }) {
+    const [showPopup, setShowPopup] = useState(false);
+
+    const containerStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        gap: '50px',
+        width: '100%',
+    };
+
     return <div>
-        This is an add order page
-        <button onClick={onReturn}>Back</button></div>
-
+        <h1>This is an add order page</h1>
+        <form onSubmit={(event) => {
+            event.preventDefault()
+            setShowPopup(true)
+            event.target.reset()
+        }}>
+            <label for="InvoiceID">Invoice ID: </label>
+            <input type="text" id="InvoiceID" name="InvoiceID" /><br /><br />
+            <div style={containerStyle}>
+                <input type="submit" value="Submit" />
+                <button onClick={onReturn}>Back</button>
+            </div>
+        </form>
+        <div className={`Popup-wrapper ${showPopup ? 'show' : ''}`}>
+            <PopUp
+                message="Submitted"
+                onClose={() => setShowPopup(false)}
+            />
+        </div>
+    </div>
 }
 
 export default AddOrderGUI
